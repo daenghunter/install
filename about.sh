a86c1ce7537c54b0d7828783a1bd1f07dd12623f
daenghunter <65704433+daenghunter@users.noreply.github.com>
2020-06-26 01:28:14 +0800

    about.sh

--- about.sh
+++ about.sh
@@ -4,4 +4,11 @@ clear
 echo -e "Script Auto Installer SSH Premium"
 echo -e "CEO, Daeng Hunter Group"
 echo -e "••••••••••••••••••••••••••••••••"
-echo -e "Spesial Thanks For
+echo -e "Spesial Thanks To:"
+echo -e "• KangArif.net"
+echo -e "• Horasss"
+echo -e "• Phreaker Sulawesi"
+echo -e "• All Member DAENG SSH"
+echo -e "••••••••••••••••••••••••••••••••"
+echo -e "Order SSH/VPS/HOSTING/DOMAIN"
+echo -e "Hubungi WhatsApp : 081342077268"
