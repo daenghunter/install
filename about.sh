bf097b5fe87314e1b21ada1be1012f6635ef7b29
daenghunter <65704433+daenghunter@users.noreply.github.com>
2020-06-26 01:20:10 +0800

    about.sh

--- about.sh
+++ about.sh
@@ -1,14 +1,7 @@
 #!/bin/bash
 
 clear
-echo -e "================================================="
-echo -e "# Script Auto Install SSH & OpenVPN             #"
-echo -e "#-----------------------------------------------#"
-echo -e "# For Debian 9 32 bit & 64 bit                  #"
-echo -e "# For VPS with KVM and VMWare virtualization    #"
-echo -e "# Modified DAENG SSH                              #"
-echo -e "#-----------------------------------------------#"
-echo -e "Terimakasih kepada semua yang telah perpartisipasi dalam membuat auto install ssh ini :)"
-echo -e ""
-echo -e "#-----------------------------------------------#"
-echo -e "================================================="
+echo -e "Script Auto Installer SSH Premium"
+echo -e "CEO, Daeng Hunter Group"
+echo -e "••••••••••••••••••••••••••••••••"
+echo -e "Spesial Thanks For
