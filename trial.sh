e01a59ca6200b3591dcc097ef36762a1f0790a14
daenghunter <65704433+daenghunter@users.noreply.github.com>
2020-06-26 00:04:46 +0800

    trial.sh

--- trial.sh
+++ trial.sh
@@ -7,7 +7,7 @@ hari="1"
 Pass=`</dev/urandom tr -dc a-f0-9 | head -c9`
 
 
-echo Script AutoCreate Akun SSH dan OpenVPN by Horas
+echo Script AutoCreate Akun SSH dan OpenVPN by Daeng Server
 sleep 1
 echo Ping Host
 echo Cek Hak Akses...
@@ -24,8 +24,9 @@ useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $Login
 exp="$(chage -l $Login | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$Pass\n$Pass\n"|passwd $Login &> /dev/null
 echo -e ""
-echo -e "Informasi SSH"
-echo -e "=========-account-=========="
+echo -e "Terimakasih Telah Menggunakan Daeng SSH Server!"
+echo -e "Demikian Detail Acoount Yang Telah Dibuat"
+echo -e "--------------------------------------------------"
 echo -e "Host: $IP" 
 echo -e "OpenSSH: 22"
 echo -e "Dropbear: 143, 110, 109, 456"
@@ -40,5 +41,5 @@ echo -e "Username: $Login "
 echo -e "Password: $Pass"
 echo -e "-----------------------------"
 echo -e "Aktif Sampai: $exp"
-echo -e "==========================="
-echo -e "Script by Horas"
+echo -e "--------------------------------------------------"
+echo -e "Script by Daeng SSH Group"
