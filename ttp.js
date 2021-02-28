a44998ede844516168d389c06fd915f2cd831f2c
daenghunter <65704433+daenghunter@users.noreply.github.com>
2021-03-01 04:29:13 +0800

    ttp.js

--- ttp.js
+++ ttp.js
@@ -1,13 +1,34 @@
-case '#ttp1':
-case '#attp':
-	   if (args.length === 0) return maxbot.reply(from, 'Mohon masukkan teksnya 💐', id)
-           const packname = 'Creator'
+case '#triggered':
+
+                if (isMedia && isImage || isQuotedImage) {
+            const packname = 'Creator'
             const author = 'Maxbot'
-                const fattp = await get.get(`https://api.xteam.xyz/attp?text=${body.slice(6)}`).json()
-                await maxbot.sendImageAsSticker(from, fattp.result, { author: '${author}', pack: `${packname}`, keepScale: true })
-                    .then(() => console.log('Success creating GIF!'))
-                    .catch(async (err) => {
-                        console.error(err)
-                        await maxbot.reply(from, 'Error!', id)
-                    })
+                    await maxbot.reply(from, ind.wait(), id)
+                    const encryptMedia = isQuotedImage ? quotedMsg : message
+                    const mediaData = await decryptMedia(encryptMedia, uaOverride)
+                    const FotoUrl = await uploadImages(mediaData, `triggered.${sender.id}`)
+                    await maxbot.sendStickerfromUrl(from, `https://lolhuman.herokuapp.com/api/editor/triggered?apikey=chika-sad-key&img=${FotoUrl}`, { author: `${author}`, pack: `${packname}`, keepScale: true })
+               } else if (quotedMsg && quotedMsg.type == 'chat') {
+                    await maxbot.reply(from, ind.wait(), id)
+                    const ppnya = await maxbot.getProfilePicFromServer(quotedMsgObj.sender.id)
+                    if (ppnya === undefined) {
+                        var ppnyaa = errorImg
+                    } else {
+                        ppnyaa = ppnya
+                    } 
+                    const datappnya = await bent('buffer')(ppnyaa)
+                    const linkppnya = await uploadImages(datappnya, `${sender.id}_ph`) 
+                    await maxbot.sendStickerfromUrl(from, `https://lolhuman.herokuapp.com/api/editor/triggered?apikey=chika-sad-key&img=${linkppnya}`, { author: `${author}`, pack: `${packname}`, keepScale: true })
+                } else {
+                    await maxbot.reply(from, ind.wait(), id)
+                    const ppRaw = await maxbot.getProfilePicFromServer(sender.id)
+                    if (ppRaw === undefined) {
+                        var ppnyaa = errorImg
+                    } else {
+                        ppnyaa = ppRaw
+                    } 
+                    const datappnya = await bent('buffer')(ppnyaa)
+                    const linkppnya = await uploadImages(datappnya, `${sender.id}_ph`) 
+                    await maxbot.sendStickerfromUrl(from, `https://lolhuman.herokuapp.com/api/editor/triggered?apikey=chika-sad-key&img=${linkppnya}`, { author: `${author}`, pack: `${packname}`, keepScale: true })
+                }
             break
